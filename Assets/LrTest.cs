2a462cbc2a97ab647778d0f305bbd4b6ff0efc4f
Mylon <binkyiv@yahoo.com>
2021-02-27 09:05:43 -0600

    Line Work

---
+++ Assets/LrTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LrTest : MonoBehaviour
+{
+    [SerializeField] private Transform[] points;
+    [SerializeField] private LineController line;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        line.SetUpLine(points);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
